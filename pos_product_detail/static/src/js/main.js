f25d8596ecee1f211623944545b0da5f87904193
tono88 <estuarsandoval@gmail.com>
2022-06-01 10:21:58 -0600

    1

--- pos_product_detail/static/src/js/main.js
+++ pos_product_detail/static/src/js/main.js
@@ -1,7 +1,7 @@
 /* Copyright (c) 2016-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>) */
 /* See LICENSE file for full copyright and licensing details. */
 /* License URL : <https://store.webkul.com/license.html/> */
-
+var exports = {};
 exports.Product = Backbone.Model.extend({
     initialize: function(attr, options){
         _.extend(this, options);
